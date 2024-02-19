53515a691024de8811b2c805bc50ae63c4d65b21
Oddux <141483016+Oddux@users.noreply.github.com>
2024-02-19 07:59:11 -0700

    Moving elements

--- SWshipyard/src/components/Container.jsx
+++ SWshipyard/src/components/Container.jsx
@@ -1,10 +1,16 @@
 import '../styles/Container.css';
 import Cards from './Cards';
+import useAllShips from '../utils/hooks';
 
 function Container() {
+    const { loading, error, data } = useAllShips();
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+    console.log(data);
+
     return (
         <div>
-            <Cards />
+            <Cards ships={data} />
         </div>
     );
 }
